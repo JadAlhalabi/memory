39b55caaad122b740b59aeaad51521a527c61d46
Jad Alward Alhalabi <0007jaal@res.ludvika.intra>
2018-09-25 15:06:57 +0200

    La till TODO kommentarer

---
+++ src/js/memory.js
@@ -0,0 +1,17 @@
+// TODO: 1.1. Design för en memory-bricka
+// TODO: 1.2. Varje bricka behöver en bild på dess baksida
+// TODO: 1.3. Varje bricka behöver en bild på dess framsida
+// TODO: 1.4. Skriva it 4x4 brickor
+// TODO: 1.5. Brickorna bör ligga i ett rutnät på 4 rader, 4 brickor per rad
+// TODO: 1.6. Man ska kunna vända en bricka genom att trycka på den
+// TODO: 1.7. Om 2 lika brickor vänds så ska dem tas bort
+// TODO: 1.8. Om 2 olika brickor vänds så ska dem vändas tillbaka
+// TODO: 1.9. När 2 brickor vänds ska antalet försök uppdateras
+// TODO: 1.10. När 2 lika brickor vänds ska antalet par uppdateras
+// TODO: 1.11. Antalet försök och antalet par ska visas för användaren
+// TODO: 1.12. Spelaren ska kunna se under hur många sekunder som han har spelat
+// TODO: 1.13. När spelet är slut ska antalet sekunder sluta räkna
+// TODO: 2. Det ska enkelt gå att ladda in flera spel, gebin att anropa en funktion flera gånger
+/* TODO: 3. En enklare dokumentation i README.md som ska vara skriven i markup språket Markdown,
+Bör inhållar kortare information om vad som ligger i respektive fil somt vilka kommandon som ska
+ köras för att starta utvecklingsserver samt hur man bygger en build. */
